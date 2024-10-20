83dc46ed5d323b3755ee5653bbfc1e3210219e39
Arseniy Belkin <arseniibelkin@gmail.com>
2024-10-20 23:04:01 +0300

    test alg for sorting

---
+++ algorithms.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <unordered_map>
+#include <vector>
+
+
+// Arseniy Belkin
+bool is_sorted(std::unordered_map<std::string, std::vector<std::string>> graph, std::vector<std::string> order);
