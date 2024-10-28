3b0c6d46775c55c83a5f8976cd1262bec93194d1
Arseniy Belkin <arseniibelkin@gmail.com>
2024-10-28 12:53:19 +0300

    edit cmake

--- topological_sort/tests/DataSetRegister.cpp
+++ topological_sort/tests/DataSetRegister.cpp
@@ -6,11 +6,11 @@
 std::queue<DataSet> GenerateDataSets(){
     return std::queue<DataSet>({
         {"empty set", {}, 1},
-        {"empty graph", readGraphFromFile("../topological_sort/tests/datasets/files/emptyGraph.txt"), 1},
-        {"one verticles graph", readGraphFromFile("../topological_sort/tests/datasets/files/oneVerticlesGraph.txt"), 1},
-        {"small correct graph", readGraphFromFile("../topological_sort/tests/datasets/files/smallCorrectGraph.txt"), 1},
-        {"large numeric correct graph", readGraphFromFile("../topological_sort/tests/datasets/files/largeCorrectGraph.txt"), 1},
-        {"small cycle graph", readGraphFromFile("../topological_sort/tests/datasets/files/smallCycleGraph.txt"), 0},
-        {"large numeric cycle graph", readGraphFromFile("../topological_sort/tests/datasets/files/largeCycleGraph.txt"), 0},
+        {"empty graph", readGraphFromFile("../../topological_sort/tests/datasets/files/emptyGraph.txt"), 1},
+        {"one vertex graph", readGraphFromFile("../../topological_sort/tests/datasets/files/oneVertexGraph.txt"), 1},
+        {"small correct graph", readGraphFromFile("../../topological_sort/tests/datasets/files/smallCorrectGraph.txt"), 1},
+        {"large numeric correct graph", readGraphFromFile("../../topological_sort/tests/datasets/files/largeCorrectGraph.txt"), 1},
+        {"small cycle graph", readGraphFromFile("../../topological_sort/tests/datasets/files/smallCycleGraph.txt"), 0},
+        {"large numeric cycle graph", readGraphFromFile("../../topological_sort/tests/datasets/files/largeCycleGraph.txt"), 0},
     });
 }
