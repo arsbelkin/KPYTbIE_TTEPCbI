3fc70e0ab6ef4961961fcfe605d634d92442e56f
ashaluba <mkvlb@mail.ru>
2024-11-07 14:21:49 +0300

    edit datasetregister, start

--- topological_sort/tests/DataSetRegister.cpp
+++ topological_sort/tests/DataSetRegister.cpp
@@ -6,11 +6,11 @@
 std::queue<DataSet> GenerateDataSets(){
     return std::queue<DataSet>({
         {"empty set", {}, 1},
-        {"empty graph", readGraphFromFile("../../topological_sort/tests/datasets/files/emptyGraph.txt"), 1},
-        {"one vertex graph", readGraphFromFile("../../topological_sort/tests/datasets/files/oneVertexGraph.txt"), 1},
-        {"small correct graph", readGraphFromFile("../../topological_sort/tests/datasets/files/smallCorrectGraph.txt"), 1},
-        {"large numeric correct graph", readGraphFromFile("../../topological_sort/tests/datasets/files/largeCorrectGraph.txt"), 1},
-        {"small cycle graph", readGraphFromFile("../../topological_sort/tests/datasets/files/smallCycleGraph.txt"), 0},
-        {"large numeric cycle graph", readGraphFromFile("../../topological_sort/tests/datasets/files/largeCycleGraph.txt"), 0},
+        {"empty graph", readGraphFromFile("./files/emptyGraph.txt"), 1},
+        {"one vertex graph", readGraphFromFile("./files/oneVertexGraph.txt"), 1},
+        {"small correct graph", readGraphFromFile("./files/smallCorrectGraph.txt"), 1},
+        {"large numeric correct graph", readGraphFromFile("./files/largeCorrectGraph.txt"), 1},
+        {"small cycle graph", readGraphFromFile("./files/smallCycleGraph.txt"), 0},
+        {"large numeric cycle graph", readGraphFromFile("./files/largeCycleGraph.txt"), 0},
     });
 }
