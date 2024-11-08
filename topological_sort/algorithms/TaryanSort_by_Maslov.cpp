f1d450c1debf26529dbdf9dde2607063911a3356
10ili <154895245+10ili@users.noreply.github.com>
2024-11-08 23:45:25 +0300

    finalcode

--- topological_sort/algorithms/TaryanSort_by_Maslov.cpp
+++ topological_sort/algorithms/TaryanSort_by_Maslov.cpp
@@ -8,14 +8,22 @@
 
 using namespace std;
 
-void dfs(string node, unordered_map<string, vector<string>>& graph, unordered_map<string, bool>& visited, stack<string>& Stack) {
+void dfs(string node, unordered_map<string, vector<string>>& graph, unordered_map<string, bool>& visited, unordered_map<string, bool> visitedway, bool& cycle, stack<string>& Stack) {
     // Помечаем текущую вершину как посещенную
     visited[node] = true;
-    
+    visitedway[node] = true;
+    if (cycle)
+        return;
     // Рекурсивно посещаем все соседние вершины
     for (auto& neighbor: graph[node]) {
         if (!visited[neighbor]) {
-            dfs(neighbor, graph, visited, Stack);
+            dfs(neighbor, graph, visited,visitedway, cycle, Stack);
+        }
+        if (visitedway[neighbor])
+        {
+            cycle = true;
+            return;
+
         }
     }
 
@@ -25,18 +33,25 @@ void dfs(string node, unordered_map<string, vector<string>>& graph, unordered_ma
 
 vector<string> TaryanSort_by_Maslov(unordered_map<string, vector<string>>& graph) {
     unordered_map<string, bool> visited;
+    unordered_map<string, bool> visitedway;
+    bool cycle = false;
     stack<string> Stack;
-
+    vector<string> sortedOrder;
     // Проходим по всем вершинам графа
     for (auto& pair : graph) {
+        if (cycle) 
+        {
+            sortedOrder.push_back("-1");
+            return sortedOrder;
+        }
         string node = pair.first;
         if (!visited[node]) {
-            dfs(node, graph, visited, Stack);
+            dfs(node, graph, visited,visitedway, cycle, Stack);
         }
     }
 
     // Извлекаем элементы из стека в вектор для получения результата
-    vector<string> sortedOrder;
+    
     while (!Stack.empty()) {
         sortedOrder.push_back(Stack.top());
         Stack.pop();
