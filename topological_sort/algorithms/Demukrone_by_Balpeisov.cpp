26cbfc82d23684bce51cf6a85cdfbc8897bc70ba
Arseniy Belkin <arseniibelkin@gmail.com>
2024-11-16 19:11:46 +0300

    O(V+E) opt

--- topological_sort/algorithms/Demukrone_by_Balpeisov.cpp
+++ topological_sort/algorithms/Demukrone_by_Balpeisov.cpp
@@ -31,27 +31,21 @@ void dfs(string node, unordered_map<string, vector<string>>& graph, unordered_ma
 vector<string> Demukrone_by_Balpeisov(unordered_map<string, vector<string>>& graph)
 {
     int n = graph.size(); //кол-во вершин
-    vector<string> vertex;  //массив с вершинами для ассоциации индексов с матрицей
-    vector<vector<int>> adj_matrix(n, vector<int>(n, 0)); //строка - начальная вершина, столбец - конечная
-
-    // !!!!!!!!!!
+    // !!!!!!!!
     unordered_map<string, int> inDegree; // входящие степени вершины
     unordered_set<string> pop_elem; // удаленные элементы
+    vector<string> sorted_graph; //сортированный граф
 
-    unordered_map<string, int> lvl_vertex; //уровни вершин графа
-    int id_sv; //индекс начальной вершины
-    int id_ev; //индекс конечной вершины
     bool cycle = false; //наличие цикла
     unordered_map<string, bool> visited; //посещённые вершины за все иттерации
     unordered_map<string, bool> visitedway; //посещённые вершины за эту иттерацию
-    int sum_col; //полустепень входаы
-    vector<string> sorted_graph, cycle_graph = {"-1"}, op_elem; //сортированный граф, для графа с циклами, удалённые вершины с нулевым уровнем
 
     for (auto& pair: graph)     
     {
         if (cycle) 
         {
-            return cycle_graph;
+            sorted_graph.push_back("-1");
+            return sorted_graph;
         }
         string node = pair.first;
         if (!visited[node]) {
@@ -72,13 +66,14 @@ vector<string> Demukrone_by_Balpeisov(unordered_map<string, vector<string>>& gra
     {
         for (auto& pair: inDegree){
             if (pair.second == 0){
-                pop_elem.emplace(pair.first);
-                sorted_graph.push_back(pair.first);
-                if (pop_elem.count(pair.first) > 0){
+                if (pop_elem.count(pair.first) == 0){
+                    sorted_graph.push_back(pair.first);
                     for (auto& v: graph[pair.first]){
                         --inDegree[v];
                     }
                 }
+
+                pop_elem.emplace(pair.first);
             }
         }
     }
