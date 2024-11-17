388e215ebc0bd1aca25a9cd24e09ad23f4348641
ashaluba <mkvlb@mail.ru>
2024-11-17 16:13:09 +0300

    edit for large graph

--- topological_sort/algorithms/dfs_by_Makkoveeva.cpp
+++ topological_sort/algorithms/dfs_by_Makkoveeva.cpp
@@ -2,34 +2,26 @@
 #include <unordered_map>
 #include<vector>
 #include <iostream>
+#include <string>
+#include<stack>
+#include <algorithm>
 
 using namespace std;
-
 
 vector<string> dfs_by_Makkoveeva(unordered_map<string, vector<string>> &graph) 
 {
 	vector<string>sortedgraph;
 	unordered_map<string, int>visited;
-	vector<string>nodes;
+	stack<string>s;
 	for (auto& pair:graph)
 	{
-		nodes.push_back(pair.first);
-	}
-	for (string& node:nodes)
-	{
+		const string& node=pair.first;
 		if (visited[node] == 0)
 		{
-			vector<string>path;
-			unordered_map<string, int>currentpath;
-			path.push_back(node);
-			currentpath[node] = 1;
-			while (!path.empty())
+			s.push(node);
+			while (!s.empty())
 			{
-				string currentnode = path.back();
-				if (visited[currentnode] == 1)
-				{
-					return { " - 1" };
-				}
+				string currentnode = s.top();
 				if (visited[currentnode] == 0)
 				{
 					visited[currentnode] = 1;
@@ -37,20 +29,23 @@ vector<string> dfs_by_Makkoveeva(unordered_map<string, vector<string>> &graph)
 					{
 						if (visited[neighbor] == 0)
 						{
-							path.push_back(neighbor);
-							currentpath[neighbor] = 1;
+							s.push(neighbor);
+						}
+						else if (visited[neighbor])
+						{
+							return { "-1" };
 						}
 					}
 				}
-				if (currentpath[currentnode] == 1)
+				else if (visited[currentnode] == 1)
 				{
-					currentpath[currentnode] = 2;
+					visited[currentnode] = 2;
 					sortedgraph.push_back(currentnode);
-					path.pop_back();
+					s.pop();
 				}
 			}
 		}
 	}
-	reverse(sortedgraph.begin(), sortedgraph.end());
+	reverse(sortedgraph.begin(),sortedgraph.end());
 	return sortedgraph;
 }
