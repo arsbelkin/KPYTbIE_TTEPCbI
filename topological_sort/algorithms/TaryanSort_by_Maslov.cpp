c51436a130ca7455d7a1b982c240a8d500450a83
10ili <154895245+10ili@users.noreply.github.com>
2024-11-20 21:52:57 +0300

    Delete coments

--- topological_sort/algorithms/TaryanSort_by_Maslov.cpp
+++ topological_sort/algorithms/TaryanSort_by_Maslov.cpp
@@ -7,10 +7,8 @@
 using namespace std;
 
 void dfs(string node, unordered_map<string, vector<string>>& graph, unordered_map<string, bool>& visited, unordered_map<string, bool> visitedway, bool& cycle, stack<string>& Stack) {
-    // Помечаем текущую вершину как посещенную
     visited[node] = true;
     visitedway[node] = true;
-    // Рекурсивно посещаем все соседние вершины
     for (auto& neighbor: graph[node]) {
         if (cycle)
             return;
@@ -23,10 +21,7 @@ void dfs(string node, unordered_map<string, vector<string>>& graph, unordered_ma
         if (!visited[neighbor]) {
             dfs(neighbor, graph, visited,visitedway, cycle, Stack);
         }
-
     }
-
-    // Добавляем текущую вершину в стек после посещения всех соседей
     Stack.push(node);
 }
 
@@ -36,7 +31,6 @@ vector<string> TaryanSort_by_Maslov(unordered_map<string, vector<string>>& graph
     bool cycle = false;
     stack<string> Stack;
     vector<string> sortedOrder;
-    // Проходим по всем вершинам графа
     for (auto& pair : graph) {
         if (cycle) 
         {
@@ -47,13 +41,9 @@ vector<string> TaryanSort_by_Maslov(unordered_map<string, vector<string>>& graph
             dfs(pair.first, graph, visited,visitedway, cycle, Stack);
         }
     }
-
-    // Извлекаем элементы из стека в вектор для получения результата
-    
     while (!Stack.empty()) {
         sortedOrder.push_back(Stack.top());
         Stack.pop();
     }
-
     return sortedOrder;
 }
